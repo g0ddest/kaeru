30b8c8208b89c058a695724a52b97d2268c6da0f
Vitaliy Velikodniy <vitaliy@velikodniy.name>
2026-09-19 15:01:17 +0300

    chore(release): 0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "app.kaeru"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.5.1"
+        versionCode = 9
+        versionName = "0.6.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "SHIKIMORI_CLIENT_ID", secret("SHIKIMORI_CLIENT_ID"))
         buildConfigField("String", "KODIK_TOKEN", secret("KODIK_TOKEN"))
